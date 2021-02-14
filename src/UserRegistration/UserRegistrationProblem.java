21fd2f04432568cbfbff0292e0338cd0b36859d0
vinayakentc <vinayakchavanentc@gmail.com>
2021-02-14 17:56:17 +0530

    enter a valid first name

--- src/UserRegistration/UserRegistrationProblem.java
+++ src/UserRegistration/UserRegistrationProblem.java
@@ -1,5 +1,13 @@
 package UserRegistration;
 
-public class UserRegistrationProblem {
 
+import java.util.regex.Pattern;
+
+public class UserRegistrationProblem {
+    private static final String NAME_PATTERN = "^([A-Z][a-zA-Z]{2,})$";
+
+    public boolean validateName(String Name) {
+        Pattern pattern =Pattern.compile(NAME_PATTERN);
+        return pattern.matcher(Name).matches();
+    }
 }

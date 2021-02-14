abe059664d335c6e431c6a405fdaa4eb978625cd
vinayakentc <vinayakchavanentc@gmail.com>
2021-02-14 18:05:03 +0530

    enter a valid last name

--- src/UserRegistration/UserRegistrationProblem.java
+++ src/UserRegistration/UserRegistrationProblem.java
@@ -4,6 +4,7 @@ package UserRegistration;
 import java.util.regex.Pattern;
 
 public class UserRegistrationProblem {
+    //Pattern used for both first name and last name
     private static final String NAME_PATTERN = "^([A-Z][a-zA-Z]{2,})$";
 
     public boolean validateName(String Name) {

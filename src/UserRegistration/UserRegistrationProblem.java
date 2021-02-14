3a1f0b7ce561a92d3c762aa920d6ca20b6f4a43f
vinayakentc <vinayakchavanentc@gmail.com>
2021-02-14 18:45:37 +0530

    follow pre defined password format

--- src/UserRegistration/UserRegistrationProblem.java
+++ src/UserRegistration/UserRegistrationProblem.java
@@ -1,13 +1,15 @@
 package UserRegistration;
 
 
+
 import java.util.regex.Pattern;
 
 public class UserRegistrationProblem {
-    //Pattern used for both first name and last name
+	
     private static final String NAME_PATTERN = "^([A-Z][a-zA-Z]{2,})$";
     private static final String EMAIL_PATTERN = "^[0-9A-Za-z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[0-9a-zA-Z]{2,4}([.][a-zA-Z]{2})*$";
-    private static final String MOBILE_PATTERN = "^([1-9]{1}([-][1-9]{1}[0-9]{1,2}||[0-9]{1,3})) {1}[1-9]{1}[0-9]{9}$";
+    private static final String MOBILE_PATTERN = "^([1-9]{1,3}[- ]?)[0-9]{10}$";
+    private static final String PASSWORD_PATTERN = "(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\\d]){1,})(?=(.*[\\W]){1,})(?!.*\\s).{8,}$";
 
     public boolean validateName(String Name) {
         Pattern pattern =Pattern.compile(NAME_PATTERN);
@@ -20,5 +22,9 @@ public class UserRegistrationProblem {
     public boolean validateMobileNumber(String mobile) {
         Pattern pattern = Pattern.compile(MOBILE_PATTERN);
         return pattern.matcher(mobile).matches();
+    }
+    public boolean validatePassword(String password) {
+        Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
+        return pattern.matcher(password).matches();
     }
 }

bff678b79d867fde58cead03b03afcf087876652
vinayakentc <vinayakchavanentc@gmail.com>
2021-02-14 18:37:40 +0530

    follow pre defined mobile format

--- src/UserRegistration/UserRegistrationProblem.java
+++ src/UserRegistration/UserRegistrationProblem.java
@@ -1,11 +1,13 @@
 package UserRegistration;
 
+
 import java.util.regex.Pattern;
 
 public class UserRegistrationProblem {
     //Pattern used for both first name and last name
     private static final String NAME_PATTERN = "^([A-Z][a-zA-Z]{2,})$";
     private static final String EMAIL_PATTERN = "^[0-9A-Za-z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[0-9a-zA-Z]{2,4}([.][a-zA-Z]{2})*$";
+    private static final String MOBILE_PATTERN = "^([1-9]{1}([-][1-9]{1}[0-9]{1,2}||[0-9]{1,3})) {1}[1-9]{1}[0-9]{9}$";
 
     public boolean validateName(String Name) {
         Pattern pattern =Pattern.compile(NAME_PATTERN);
@@ -14,5 +16,9 @@ public class UserRegistrationProblem {
     public boolean validateEmail(String email) {
         Pattern pattern = Pattern.compile(EMAIL_PATTERN);
         return pattern.matcher(email).matches();
+    }
+    public boolean validateMobileNumber(String mobile) {
+        Pattern pattern = Pattern.compile(MOBILE_PATTERN);
+        return pattern.matcher(mobile).matches();
     }
 }
